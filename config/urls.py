43e0ffb7b39dc9b16ff70b684ad575d45986a048
rodolphorocha <rodolpho.r2008@gmail.com>
2025-06-26 19:59:44 -0300

    FIX: consertado o bug no servidor

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import (
+from clinica.views import (
     EspecialidadeViewSet,
     MedicoViewSet,
     PacienteViewSet,
